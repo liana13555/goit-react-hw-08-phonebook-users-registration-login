4b739f5b89ac2643e1d23917c76aeee0ee460993
Liana Baryshova <barishovaliana@gmail.com>
2021-10-20 22:39:57 +0200

    Refactoring with Re-export

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { deleteContact } from '../../redux/actions';
-import * as contactsOperations from '../../redux/operations';
-import { getVisibleContacts } from '../../redux/selectors';
+import { deleteContact } from '../../redux/contacts/contacts-actions';
+import {contactsOperations, contactsSelectors} from '../../redux/contacts';
 import { List, ListItem, ButtonDelete, Span } from './ContactList.styled';
 
 
 export default function ContactList() {
     const dispatch = useDispatch();
-    const contacts = useSelector(getVisibleContacts);
+    const contacts = useSelector(contactsSelectors.getVisibleContacts);
     const onDeleteContact = id => dispatch(deleteContact(id));
     
     useEffect(() => {
