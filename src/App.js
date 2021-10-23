3f2d168409f1dd37080a2733aa590959e65e1190
Liana Baryshova <barishovaliana@gmail.com>
2021-10-23 23:19:44 +0200

    Update files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 // import { useEffect } from 'react';
 // import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
-import AppBar from '../AppBar';
-import { Container } from './App.styled';
-import ContactsView from '../../views/ContactsView';
-import HomeView from '../../views/HomeView';
-import RegisterView from '../../views/RegisterView';
-import LoginView from '../../views/LoginView';
+import AppBar from './components/AppBar';
+import Container from './components/Container';
+import ContactsView from './views/ContactsView';
+import HomeView from './views/HomeView';
+import RegisterView from './views/RegisterView';
+import LoginView from './views/LoginView';
 // import { authOperations } from '../../redux/auth/auth-operations';
 
 export default function App() {
- 
   return (
     <Container>
       <AppBar />
@@ -20,7 +19,6 @@ export default function App() {
         <Route path="/register" component={RegisterView} />
         <Route path="/login" component={LoginView} />
         <Route path="/contacts" component={ContactsView} />
-      
       </Switch>
     </Container>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// import Button from 'react-bootstrap/Button';
 
 const Navigation = () => (
   <nav>
