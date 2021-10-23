59bbe237cb040d6b1241a7589cd22314545f3573
Liana Baryshova <barishovaliana@gmail.com>
2021-10-23 23:18:38 +0200

    Add React Bootstrap

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import './index.css';
-import App from './components/App/App.jsx';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
