8e8024637c817dbabe05189dc7b7eb751e82bb2d
Liana Baryshova <barishovaliana@gmail.com>
2021-10-23 22:23:30 +0200

    Add redux persist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,27 @@
-import SectionTitle from '../SectionTitle';
-import ContactForm from '../ContactForm';
-import Filter from '../Filter';
-import ContactList from '../ContactList';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import AppBar from '../AppBar';
 import { Container } from './App.styled';
+import ContactsView from '../../views/ContactsView';
+import HomeView from '../../views/HomeView';
+import RegisterView from '../../views/RegisterView';
+import LoginView from '../../views/LoginView';
+// import { authOperations } from '../../redux/auth/auth-operations';
 
 export default function App() {
+ 
   return (
     <Container>
-      <SectionTitle title="Phonebook">
-        <ContactForm />
-      </SectionTitle>
+      <AppBar />
+
+      <Switch>
+        <Route exact path="/" component={HomeView} />
+        <Route path="/register" component={RegisterView} />
+        <Route path="/login" component={LoginView} />
+        <Route path="/contacts" component={ContactsView} />
       
-      <SectionTitle title="Contacts">
-        <Filter/>
-        <ContactList/>
-      </SectionTitle>
+      </Switch>
     </Container>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import contactReducer from '../redux/contacts/contacts-reducer';
+import { authReducer } from './auth';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactReducer,
   },
-
-  devTools: process.env.NODE_ENV !== 'production',
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
