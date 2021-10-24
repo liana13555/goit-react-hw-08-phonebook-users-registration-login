db2976b88f1525030a4de2634be99bc940cad439
Liana Baryshova <barishovaliana@gmail.com>
2021-10-24 18:08:35 +0200

    Add UserMenu with styles

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelectors, authOperations } from '../../redux/auth';
+import defaultAvatar from './default-avatar.png';
+import { Thumb, Avatar, Name } from './UserMenu.styled';
+
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUsername);
+  const avatar = defaultAvatar;
+
+  return (
+    <Thumb>
+      <Avatar src={avatar} alt="" width="32" />
+      <Name >Добро пожаловать, {name}</Name>
+      <button type="button" onClick={() => dispatch(authOperations.logOut())}>
+        Выйти
+      </button>
+    </Thumb>
+  );
+}
---
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Thumb = styled.div`
+  display: 'flex';
+  align-items: 'center';
+`;
+
+export const Avatar = styled.img`
+  margin-right: 4px;
+`;
+
+export const Name = styled.span`
+  margin-right: 12px;
+  font-weight: 700;
+`;
