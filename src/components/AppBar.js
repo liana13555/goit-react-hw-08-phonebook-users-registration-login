786965d23c3ead8fab76670961bceb997ddd555a
Liana Baryshova <barishovaliana@gmail.com>
2021-10-25 10:13:56 +0200

    Add bootstrap's components

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,13 +3,29 @@ import Navigation from './Navigation';
 import UserMenu from './UserMenu';
 import AuthNav from './AuthNav';
 import { authSelectors } from '../redux/auth';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+
+// const styles = {
+//   header: {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     borderBottom: '1px solid #2A363B',
+//   },
+// };
 
 export default function AppBar() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
-    <header>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navigation />
+        <Nav className="ml-auto">{isLoggedIn ? <UserMenu /> : <AuthNav />}</Nav>
+      </Container>
+    </Navbar>
+    // <header style={styles.header}>
+    //   <Navigation />
+    //   {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    // </header>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Nav, Navbar, Container } from 'react-bootstrap';
+//
 
 const Navigation = () => (
-  <Navbar bg="primary" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">Phonebook</Navbar.Brand>
-
-      <Nav className="ml-auto">
-        {/* <NavLink to="/">Home</NavLink>
-        <NavLink to="/contacts" exact>Contacts</NavLink> */}
-        <Nav.Link to="/" href="#home">
-          Home
-        </Nav.Link>
-        <Nav.Link to="/contacts" exact href="#registration">
-          Registration
-        </Nav.Link>
-        <Nav.Link href="#login">Login</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
+  <nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/contacts" exact>
+      Contacts
+    </NavLink>
+  </nav>
 );
 
 export default Navigation;
 
-<Nav>
+// eslint-disable-next-line
+{
+  /* <Nav>
   <NavLink to="/">Home</NavLink>
 
   <NavLink to="/contacts" exact>
     Contacts
   </NavLink>
-</Nav>;
+</Nav>; */
+}
