83f2413264a1a5b248b56d9ddc1a744614775770
Liana Baryshova <barishovaliana@gmail.com>
2021-10-25 10:16:18 +0200

    Update styles

--- src/views/HomeView/HomeView.styled.jsx
+++ src/views/HomeView/HomeView.styled.jsx
@@ -1,10 +1,20 @@
 import styled from '@emotion/styled';
+import img from './image/phone3.jpg';
 
 export const Thumb = styled.div`
    min-height: 'calc(100vh - 50px)';
    display: flex;
-   align-items: center;
+   /* align-items: center; */
    justify-content: center;
+   margin-left: auto;
+   margin-right: auto;
+   margin-top: 50px;
+   background-image: url(${img});
+   max-width: 700px;
+   height: 500px;
+   background-size: cover;
+   background-repeat: no-repeat;
+   /* background-position: center;  */
 `;
 
 export const Title = styled.h1`
