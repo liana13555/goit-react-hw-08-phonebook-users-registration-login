1e3ee22fa9ff0570fb1ea2f144430ae9843bb0f4
Liana Baryshova <barishovaliana@gmail.com>
2021-10-26 10:29:45 +0200

    Update code for Contactview

--- src/views/ContactsView/ContactsView.js
+++ src/views/ContactsView/ContactsView.js
@@ -1,19 +1,22 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { fetchContactsOperations } from '../../redux/contacts/contacts-operations';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import contactsOperations from '../../redux/contacts/contacts-operations';
 import ContactForm from '../../components/ContactForm';
 import Filter from '../../components/Filter';
 import ContactList from '../../components/ContactList';
+import contactsSelectors from '../../redux/contacts/contacts-selectors';
 
 export default function ContactsView(params) {
-  //   const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const isLoadingContacts = useSelector(contactsSelectors.getLoading);
 
-  //   useEffect(() => dispatch(fetchContactsOperations()), [dispatch]);
+  useEffect(() => dispatch(contactsOperations.fetchContacts()), [dispatch]);
 
   return (
     <div>
       <ContactForm />
       <Filter />
+      {isLoadingContacts && <h1>Loading...</h1>}
       <ContactList />
     </div>
   );
