383a0692f5848e3e2263bcdf2e4dd1c6c6c7523a
Liana Baryshova <barishovaliana@gmail.com>
2021-10-29 09:13:19 +0200

    Update Homeview styles

--- src/views/HomeView/HomeView.styled.jsx
+++ src/views/HomeView/HomeView.styled.jsx
@@ -4,23 +4,17 @@ import img from './image/phone3.jpg';
 export const Thumb = styled.div`
    min-height: 'calc(100vh - 50px)';
    display: flex;
-   /* align-items: center; */
    justify-content: center;
    margin-left: auto;
    margin-right: auto;
-   background-image: linear-gradient(
-    to right,
-    rgba(253, 254, 255, 0.4),
-    rgba(249, 249, 253, 0.4)),
-    url(${img});
-
    /* background-image: url(${img}); */
-   max-width: 800px;
-   height: 600px;
+
+   max-width: 440px;
+   height: 320px;
    background-size: cover;
    background-repeat: no-repeat;
 `;
 
-export const Title = styled.h2`
+export const Title = styled.h3`
    margin-top: 10px;
 `;
